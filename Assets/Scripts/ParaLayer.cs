484135b28938ed7e7c206a76e124858f134134fe
Persy <persovegor@gmail.com>
2021-11-07 13:57:35 +0700

    Parallax

--- Assets/Scripts/ParaLayer.cs
+++ Assets/Scripts/ParaLayer.cs
@@ -4,4 +4,31 @@ using UnityEngine;
 
 public class ParaLayer : MonoBehaviour
 {
+	public float length = 19.2f;
+	public float factor = 1.0f;
+
+	public float startPos;
+
+	float pos = 0;
+	public void Start()
+	{
+		pos = startPos;
+		Move();
+	}
+	public void Move(float delta = 0f)
+	{
+		delta *= factor;
+		pos += delta;
+		while(pos >= length)
+		{
+			pos -= length;
+		}
+		while(pos < -length)
+		{
+			pos += length;
+		}
+		Vector3 p = transform.position;
+		p.x = pos;
+		transform.position = p;
+	}
 }
